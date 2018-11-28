212e7cc4e19308b8710fac2d0efae9f89d120005
manabshy <manabshy@gmail.com>
2018-11-28 19:20:11 +0000

    Fix: App module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ButtonComponent } from './button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ButtonComponent
   ],
   imports: [
     BrowserModule
