2317291bb16c780ef1b72f0a03b1283a18bf61b9
manabshy <manabshy@gmail.com>
2018-11-28 17:43:40 +0000

    First Initial Commit

---
+++ src/stories/index.ts
@@ -0,0 +1,18 @@
+import { storiesOf } from '@storybook/angular';
+import { action } from '@storybook/addon-actions';
+import { ButtonComponent } from '../app/button/button.component';
+
+storiesOf('My Button', module)
+  .add('with some emoji', () => ({
+    component: ButtonComponent,
+    props: {
+      text: '😀 😎 👍 💯',
+    },
+  }))
+  .add('with some emoji and action', () => ({
+    component: ButtonComponent,
+    props: {
+      text: '😀 😎 👍 💯',
+      click: action('clicked'),
+    },
+  }));
